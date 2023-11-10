68bfad8d1fec50f51c735f8b9759c34c2fea1128
corinachistol <chistolcorina19@gmail.com>
2023-11-10 14:13:28 +0200

    displaying lists of people

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
-import { SplitScreen } from "./SplitScreen";
+import { RegularList } from "./RegularList";
+import { LargePersonListItem } from "./people/LargePersonListItem";
+import { SmallPersonListItem } from "./people/SmallPersonListItem";
 
+const people =[{
+  name: "John Doe",
+  age:54,
+  hairColor: "brown",
+  hobbies:['swimming', 'bicycling', 'video games'],
+}, {
+  name: "Brenda Doe",
+  age:45,
+  hairColor: "black",
+  hobbies:['golf', 'math'],
+}, {
+  name: "Jane Doe",
+  age:27,
+  hairColor: "blonde",
+  hobbies:['biology', 'maedicine', 'gymanstics'],
+}]
 
-const LeftHandComponent = ({name}) => {
-  return <h1 style={{backgroundColor:"green"}}>{name}!</h1>
-}
-
-const RightHandComponent = ({message}) => {
-  return <p  style={{backgroundColor:"red"}}>{message}!</p>
-}
+const products =[{
+  name:"Flat-Screen Tv",
+  price:"$300",
+  description: "Huge LCS screen, a great deal",
+  rating:4.5
+}, {
+  name:"Basketball",
+  price:"$10",
+  description: "Like the pros use",
+  rating:3.8
+}, {
+  name:"running Shoes",
+  price:"$120",
+  description: "Like the pros use",
+  rating:4.2
+}]
 
 function App() {
   return (
-   <SplitScreen leftWeight={1} rightWeight={3}>
-      <LeftHandComponent name="Shaun"/>
-      <RightHandComponent message="Hello"/>
-    </SplitScreen>
+    <>
+    <RegularList
+      items={people}
+      resourceName="person"
+      itemComponent={SmallPersonListItem}/>
+    <RegularList
+      items={people}
+      resourceName="person"
+      itemComponent={LargePersonListItem}/>
+    </>
   )
 }
 
---
+++ src/RegularList.js
@@ -0,0 +1,13 @@
+export const RegularList= ({
+    items,
+    resourceName,
+    itemComponent: ItemComponent 
+}) => {
+    return(
+        <>
+        {items.map((item,index) => {
+            return <ItemComponent key={index} {...{ [resourceName]: item} } />
+        } )}
+        </>
+    )
+}
